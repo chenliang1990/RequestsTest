91b6a0f0c1bec94b9b99f46cb0627419129a3097
chenliang <137128361@qq.com>
2020-08-10 14:40:18 +0800

    111

--- demo3.py
+++ demo3.py
@@ -7,7 +7,7 @@ from dbtools import query,commit
 u = "http://192.144.148.91:2333/login"
 d = {"username":"liuyun1", "password":"a12345678"}
 res = requests.post(url = u,json = d)
-# print(res.text)
+print(res.text)
 
 #判断结果
 assert res.status_code == 200
@@ -15,6 +15,7 @@ assert res.json()["status"] == 200
 
 #查询数据库
 sql = "select * from t_user where username = '{}'".format(d["username"])
+print(sql)
 assert len(query(sql)) != 0
 print("用户登录接口成功")
 #取出用户token
@@ -23,27 +24,27 @@ write_file("token.txt",token)
 user_token = read_file("token.txt")
 
 
-#接口2 用户写篇文章
-#构造请求
-u1 = "http://192.144.148.91:2333/article/new"
-d1 = {"title":"如何学习测试",
-      "content":"每天通宵学习",
-      "tags":"测试1234",
-      "brief":"好好学习测试",
-      "ximg":"chen.jpg"}
-h1 = {"Content-Type":"application/json","token":user_token} #请求头:字典
-res1 = requests.post(url = u1,json = d1,headers = h1)
-# print(res1.text)
-#判断结果
-assert res1.status_code == 200
-assert res1.json()["status"] == 200
+# #接口2 用户写篇文章
+# #构造请求
+# u1 = "http://192.144.148.91:2333/article/new"
+# d1 = {"title":"如何学习测试",
+#       "content":"每天通宵学习",
+#       "tags":"测试1234",
+#       "brief":"好好学习测试",
+#       "ximg":"chen.jpg"}
+# h1 = {"Content-Type":"application/json","token":user_token} #请求头:字典
+# res1 = requests.post(url = u1,json = d1,headers = h1)
+# # print(res1.text)
+# #判断结果
+# assert res1.status_code == 200
+# assert res1.json()["status"] == 200
 
-#查询数据库 第一种写法
-# sql = "select * from t_article where title = '如何学习测试' and content = '每天通宵学习'"
+# #查询数据库 第一种写法
+# # sql = "select * from t_article where title = '如何学习测试' and content = '每天通宵学习'"
+# # assert len(query(sql)) != 0
+# # print("发表文章成功")
+# #查询数据库 第二种写法
+# articleId = res1.json()["data"]["articleid"]
+# sql = "select * from t_article where id = {}".format(articleId)
 # assert len(query(sql)) != 0
-# print("发表文章成功")
-#查询数据库 第二种写法
-articleId = res1.json()["data"]["articleid"]
-sql = "select * from t_article where id = {}".format(articleId)
-assert len(query(sql)) != 0
-print("发表文章成功!")
+# print("发表文章成功!")
--- 写日记.py
+++ 写日记.py
@@ -6,7 +6,7 @@ now = time.strftime("%y-%m-%d %H:%M:%S")
 #将文件内容写进去
 #"日记.txt"是相对位置，也可以把写入的文件存入F盘，可以写"F:\日记.txt"
 text = input("请输入内容: ")
-with open("日记.txt","a",encoding = "utf8") as f:
+with open("日记.txt","a",encoding = "utf8") as f: #a是追加在后面，r是只读，w是只写，在继续写会覆盖
     f.writelines(now+"\n")
     f.write(text+"\n")
     f.write("--------------------------------\n")
